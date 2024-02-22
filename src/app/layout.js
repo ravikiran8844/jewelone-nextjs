9041f8d177c863442a1ad45ba8f542eaef639088
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-02-22 16:04:15 +0530

    updated code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
 import { Inter,Great_Vibes } from 'next/font/google'
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import Navbar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import BootstrapClient from './components/BootstrapClient';
-const greatVibes = Great_Vibes({subsets: ['latin'],  weight: '400',display: 'swap',variable: '--font-great-vibes',})
+
 
 const inter = Inter({ subsets: ['latin'],  weight: ['400','500','700'], style: ['normal'],display: 'swap',variable: '--font-inter', }) 
+const greatVibes = Great_Vibes({subsets: ['latin'],  weight: '400',display: 'swap',variable: '--font-great-vibes',})
+
 
 export const metadata = {
   title: 'JewelOne',
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={`${inter.className} ${greatVibes.className}`}>
+      <body className={`${greatVibes.className} ${inter.className}`}>
         <Navbar/>
         {children}
         <Footer/>
