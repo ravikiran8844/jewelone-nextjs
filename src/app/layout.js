7b8b6aa35cc21457e3a9a66bd54d537953058b22
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-02-22 16:07:44 +0530

    updated code

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={`${greatVibes.className} ${inter.className}`}>
+      <body className={`${inter.className}`}>
         <Navbar/>
         {children}
         <Footer/>
