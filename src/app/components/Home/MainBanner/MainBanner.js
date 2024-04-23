06fe99c63b6e1a1c4c4adf1a689cba2294353798
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-04-23 16:07:15 +0530

    updated banner

--- src/app/components/Home/MainBanner/MainBanner.js
+++ src/app/components/Home/MainBanner/MainBanner.js
@@ -1,73 +1,76 @@
-"use client"
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import './MainBanner.css'
-import Image from 'next/image'
-import Link from 'next/link';
-
-
+"use client";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import "./MainBanner.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export const MainBanner = () => {
-
-
   return (
-   <section className='main-banner-section home-section1'>
-    <Splide options={ {
-                    type: 'loop',
-                    autoplay: true,
-                    interval: 4000,
-                  } }>
-              
-              <SplideSlide>
-              <Link href="/tvam">
-              <picture>
-                <source
-                    srcSet="/home-mobile-banner1.webp"
-                    type="image/webp"
-                    media="(max-width: 650px)"
-                />
-                <Image
-                    priority="true"
-                    alt="main banner"
-                    src="/home-main-banner1.webp"
-                    height={544}
-                    width={1519}
-                               />
-                </picture>
-              </Link>
+    <section className="main-banner-section home-section1">
+      <Splide
+        options={{
+          type: "loop",
+          autoplay: true,
+          interval: 4000,
+        }}
+      >
+  <SplideSlide>
+          <Link href="/akshaya-tritiya">
+            <picture>
+              <source
+                srcSet="/AkshayaTritiya/main-banner-mob-img1.webp"
+                type="image/webp"
+                media="(max-width: 650px)"
+              />
+              <Image
+                priority="true"
+                alt="main banner"
+                src="/AkshayaTritiya/main-banner-img1.webp"
+                height={544}
+                width={1519}
+              />
+            </picture>
+          </Link>
+        </SplideSlide>
 
 
+        <SplideSlide>
+          <Link href="/tvam">
+            <picture>
+              <source
+                srcSet="/home-mobile-banner1.webp"
+                type="image/webp"
+                media="(max-width: 650px)"
+              />
+              <Image
+                priority="true"
+                alt="main banner"
+                src="/home-main-banner1.webp"
+                height={544}
+                width={1519}
+              />
+            </picture>
+          </Link>
+        </SplideSlide>
 
-
-              </SplideSlide>
-
-
-           
-
-
-              <SplideSlide>
-              <picture>
-                <source
-                    srcSet="/home-main-banner3.webp"
-                    type="image/webp"
-                    media="(min-width: 650px)"
-                />
-                <Image
-                    alt="Image X alt"
-                    src="/home-mobile-banner3.webp"
-                    height={541}
-                    width={1519 }
-                    priority="true"
-
-                />
-                </picture>
-
-
-
-
-              </SplideSlide>
-             
-            </Splide>
-   </section>
-  )
-}
+        <SplideSlide>
+          <picture>
+            <source
+              srcSet="/home-main-banner3.webp"
+              type="image/webp"
+              media="(min-width: 650px)"
+            />
+            <Image
+              alt="Image X alt"
+              src="/home-mobile-banner3.webp"
+              height={541}
+              width={1519}
+              priority="true"
+            />
+          </picture>
+        </SplideSlide>
+      </Splide>
+    </section>
+  );
+};
