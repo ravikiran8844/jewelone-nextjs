19da9088f4fb391ba6b1de9778d3251109a37ac9
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-10-09 16:43:05 +0530

    updated banners

--- src/app/components/Home/MainBanner/MainBanner.js
+++ src/app/components/Home/MainBanner/MainBanner.js
@@ -23,19 +23,12 @@ export const MainBanner = () => {
               width={1920}
               height={691}
             />
-            <source
-              srcSet="/home/banner1-mob.webp"
-              type="image/webp"
-              media="(min-width: 450px)"
-              width={600}
-              height={800}
-            />
             <Image
               className="img-fluid w-100 h-auto"
               alt="Main Banner Image"
-              src="/home/banner1-mob-small.webp"
-              width={450}
-              height={600}
+              src="/home/banner1-mob.webp"
+              width={600}
+              height={800}
               sizes="100vw"
               loading="eager"
               priority
@@ -52,19 +45,13 @@ export const MainBanner = () => {
               width={1920}
               height={691}
             />
-            <source
-              srcSet="/home/jone-banner1mob.webp"
-              type="image/webp"
-              media="(min-width: 450px)"
-              width={600}
-              height={800}
-            />
-            <Image
+   
+           <Image
               className="img-fluid w-100 h-auto"
               alt="Main Banner Image"
-              src="/home/jone-banner1mob-small.webp"
-              width={450}
-              height={600}
+              src="/home/jone-banner1mob.webp"
+              width={600}
+              height={800}
               sizes="100vw"
               loading="eager"
               priority
@@ -81,19 +68,13 @@ export const MainBanner = () => {
               width={1920}
               height={691}
             />
-            <source
-              srcSet="/home/jone-banner2mob.webp"
-              type="image/webp"
-              media="(min-width: 450px)"
-              width={600}
-              height={800}
-            />
+       
             <Image
               className="img-fluid w-100 h-auto"
               alt="Main Banner Image"
-              src="/home/jone-banner2mob-small.webp"
-              width={450}
-              height={600}
+              src="/home/jone-banner2mob.webp"
+              width={600}
+              height={800}
               sizes="100vw"
               loading="eager"
               priority
