88c0a0db06670661aab0efd9ef394c4d639aef11
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-12-03 18:04:48 +0530

    updated code

--- src/app/careers/CareerForm.jsx
+++ src/app/careers/CareerForm.jsx
@@ -107,8 +107,8 @@ const CareerForm = () => {
         <div className="careers-form">
               <div className="card bg-transparent border-0">
                 <div className="card-body">
-                  <div className="fs-3 fw-semibold text-white mb-4">Submit your profile</div>
-                <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column gap-3">
+                  <div className="fs-4 fw-semibold text-white mb-3">Submit your profile</div>
+                <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column gap-2">
                 <div>
                   <input
                   required
@@ -174,8 +174,8 @@ const CareerForm = () => {
                 </div>
 
                 <div>
-                  <div className="mb-2" style={{ color: "#F4D86C" }}>Upload Your Resume</div>
-                  <div className="mb-2 text-white">Resume</div>
+                  <div className="mb-1" style={{ color: "#F4D86C" }}>Upload Your Resume</div>
+                  <div className="mb-1 text-white">Resume</div>
                   <div>
                     <label htmlFor="resume" className="upload-resume-input">Browse</label>
                   </div>
@@ -188,12 +188,12 @@ const CareerForm = () => {
                   />
                   <div>
                   {resumeName && (
-                  <div className="mt-2 text-white">
+                  <div className="mt-1 text-white">
                     Selected File: <strong>{resumeName}</strong>
                   </div>
                 )}
                   </div>
-                  <div className="d-flex align-items-center gap-1 mt-2 text-white" style={{ fontSize: "14px" }}>
+                  <div className="d-flex align-items-center gap-1 mt-1 text-white" style={{ fontSize: "12px" }}>
                   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24" fill="currentColor" className="text-white">
                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
                   </svg>
@@ -217,7 +217,7 @@ const CareerForm = () => {
                 </div>
 
                 <div>
-                  <button type="submit" className="btn btn-warning px-4">
+                  <button type="submit" className="btn btn-warning px-4 mt-2">
                     {loading ? <span className="spinner-border spinner-border-sm" aria-hidden="true"></span> : <span>SUBMIT</span>}
                   </button>
                 </div>
--- src/app/careers/CareersPageSection1.jsx
+++ src/app/careers/CareersPageSection1.jsx
@@ -5,11 +5,10 @@ import CareerForm from "./CareerForm";
 const CareersPageSection1 = () => {
   return (
     <div className="bg-red">
-      <div className="position-relative d-none d-lg-block">
+      <div className="position-relative d-none d-lg-block" >
         <div>
           <Image
             className="img-fluid"
-            style={{ minHeight: "100vh" }}
             src="/careers/section1-banner.webp"
             width={1920}
             height={860}
