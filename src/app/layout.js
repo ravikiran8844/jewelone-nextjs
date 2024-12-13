451b3705284dbd9f7df8217392d5cfde58c5a54d
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-12-13 11:13:48 +0530

    updated gtag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,32 @@
-import { Inter, Great_Vibes } from 'next/font/google'
-import './globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Inter, Great_Vibes } from "next/font/google";
+import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import Navbar from './components/NavBar/NavBar';
 // import Footer from './components/Footer/Footer';
-import BootstrapClient from './components/BootstrapClient';
+import BootstrapClient from "./components/BootstrapClient";
 // import GsapBall from './components/GsapBall';
 // import { GoogleAnalytics } from '@next/third-parties/google'
-import { GoogleTagManager } from '@next/third-parties/google' 
+import { GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
 
-
-const inter = Inter({ subsets: ['latin'],  weight: ['400','500'], style: ['normal'],display: 'swap',variable: '--font-inter', }) 
-const great_vibes = Great_Vibes({subsets: ['latin'],  weight: '400',display: 'swap',variable: '--font-great-vibes',})
-
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  style: ["normal"],
+  display: "swap",
+  variable: "--font-inter",
+});
+const great_vibes = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-great-vibes",
+});
 
 export const metadata = {
-  title: 'JewelOne',
-  description: 'JewelOne Website',
-}
-
-
+  title: "JewelOne",
+  description: "JewelOne Website",
+};
 
 // async function getData() {
 //   const res = await fetch('http://emerald-jewel-career.brightbridge.co/wp-json/acf/v3/options/option');
@@ -28,37 +35,32 @@ export const metadata = {
 //     // This will activate the closest `error.js` Error Boundary
 //     throw new Error('Failed to fetch data')
 //   }
-  
- 
+
 //   return res.json()
 // }
 
-
 export default async function RootLayout({ children }) {
-
   return (
     <html lang="en">
-      <body  className={`${inter.variable} ${great_vibes.variable}` }>
+      <body className={`${inter.variable} ${great_vibes.variable}`}>
         {/* <Navbar/> */}
         {children}
         {/* <Footer/> */}
-        <BootstrapClient/>
+        <BootstrapClient />
         {/* <GsapBall/> */}
-        </body>
-        {/* <GoogleTagManager gtmId="GTM-KRZSB3WW" /> */}
-
-      
-      <Script  id='google-tag-manager'
+        <Script
+        id="google-tag-manager"
         strategy="worker"
         dangerouslySetInnerHTML={{
-          __html: `(function (w, d, s, l, i) {
-                    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-                    var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                    j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', 'GTM-XXXXXX');`
-      }}
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KRZSB3WW');`,
+        }}
       />
-
+      </body>
+      {/* <GoogleTagManager gtmId="GTM-KRZSB3WW" /> */}
     </html>
-  )
+  );
 }
